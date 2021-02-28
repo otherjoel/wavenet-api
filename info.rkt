720cd6edd399cceb543e5626e9a3aa9d8f53de3c
Joel Dueck <joel@jdueck.net>
2021-02-28 13:05:28 -0600

    Update dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,10 @@
 #lang info
 (define collection "wavenet")
 (define deps '("base" "hash-view-lib" "http-easy"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("gui-doc"
+                     "gui-lib"
+                     "hash-view"
+                     "scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/wavenet.scrbl" ())))
 (define pkg-desc "Racket interface for Google’s Wavenet Cloud text-to-speech API")
 (define version "0.0")
